6c71981a5af1c2479dc73428c83ae069dde03a30
aritabryan <147879047+aritabryan@users.noreply.github.com>
2024-02-27 22:49:41 -0600

    Update Tarea3Repaso.java

--- Tarea #3 Repaso/src/tarea/pkg3/repaso/Tarea3Repaso.java
+++ Tarea #3 Repaso/src/tarea/pkg3/repaso/Tarea3Repaso.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+//Bryan Jose Arita Lopez  y Gabriel Alexis Mejia
 package tarea.pkg3.repaso;
 
 import java.util.Random;
